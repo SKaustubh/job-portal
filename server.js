e43739831bb11797efb6f6c983bb1343af4ab19b
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-15 01:41:16 +0530

    mvc patterns created and dotenv files configured

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import colors from 'colors'
+//dot config
+dotenv.config()
+
 
 //rest object
 const app = express();
 
 //routes
 app.get('/',(req,res)=>{
-    res.send("<h1>hello</h1>");
+    res.send("<h1>hello ks</h1>");
 });
 
+//port
+const PORT = process.env.PORT || 2524;
+
 // listen
-app.listen(5000,()=>{
-    console.log("node server started");
+app.listen(PORT,()=>{
+    console.log(`node server running in ${process.env.DEV_MODE} mode and server started at ${PORT}`);
 })
