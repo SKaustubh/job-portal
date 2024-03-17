d24bd45c9eeb60a2f54a190b015802fcca2de405
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-17 18:02:51 +0530

    update api craeted

--- models/userModel.js
+++ models/userModel.js
@@ -1,52 +1,53 @@
-import mongoose from 'mongoose'
-import validator from 'validator'
-import bcrypt from 'bcryptjs'
-import JWT from 'jsonwebtoken'
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 //schema
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: 'string',
-        required: [true, 'Name is required']
-    },
-    email: {
-        type: 'string',
-        required: [true, 'Email is required'],
-        unique: true,
-        validate: validator.isEmail
-    },
-    password: {
-        type: 'string',
-        required: [true, 'Password is required'],
-        minlength: [6, 'password must be at least 6 characters'],
-        select:true,
+      type: String,
+      required: [true, "Name Is Require"],
     },
     lastName: {
-        type: 'string',
-
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, " Email is Require"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: [true, "password is require"],
+      minlength: [6, "Password length should be greater than 6 character"],
+      select: true,
     },
     location: {
-        type: 'string',
-        default: 'India'
-    }
-}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } })
-
-
-//middlewares
-userSchema.pre('save', async function () {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
+      type: String,
+      default: "India",
+    },
+  },
+  { timestamps: true }
+);
+// middelwares
+userSchema.pre("save", async function () {
+  if (!this.isModified) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-// comapre password
-userSchema.methods.comparePassword = async function (userPassword){
-    const isMatch =await bcrypt.compare(userPassword,this.password)
-    return isMatch 
-}
-
-//json webtokens
-userSchema.methods.createJWT = function () {
-    return JWT.sign({ userID: this._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+//compare password
+userSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
 };
 
-export default mongoose.model('User', userSchema)
+//JSON WEBTOKEN
+userSchema.methods.createJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+export default mongoose.model("User", userSchema);
