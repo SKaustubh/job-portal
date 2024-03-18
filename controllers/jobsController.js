6f922e3da51cc34dd84da6886fb50b2d35954934
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-18 18:30:15 +0530

    delete job created

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -4,19 +4,66 @@ import mongoose from "mongoose";
 export const createJobController = async (req, res, next) => {
     const { company, position } = req.body;
     if (!company || !position) {
-      next("Please Provide All Fields");
+        next("Please Provide All Fields");
     }
     req.body.createdBy = req.user.userId;
     const job = await jobsModel.create(req.body);
     res.status(201).json({ job });
-  };
+};
 
-  // ====== Get JObs controller=======
+// ====== Get JObs controller=======
 
-  export const getAllJobsController = async (req,res,next) => {
-    const jobs= await jobsModel.find({createdBy:req.user.userId});
+export const getAllJobsController = async (req, res, next) => {
+    const jobs = await jobsModel.find({ createdBy: req.user.userId });
     res.status(200).json({
         totalJobs: jobs.length,
         jobs
     })
-  }
+}
+
+// ======= update jobs ====
+
+export const updateJobController = async (req, res, next) => {
+    const { id } = req.params;
+    const { company, position } = req.body
+
+    // validation
+    if (!company || !position) {
+        next("Please provide all fields")
+    }
+    // find jobs
+    const job = await jobsModel.findOne({ _id: id })
+    // validation for job
+    if (!job) {
+        next(`no job found with this id ${id}`)
+    }
+    if (!req.user.userId === job.createdBy.toString()) {
+        next("You are not authorized to update this job")
+        return
+    }
+    const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+        new: true,
+        runValidators: true // built in validators function
+    })
+    // res
+    res.status(200).json({ updateJob })
+}
+
+
+// === deleteJob ===
+
+export const deleteJobController =async (req, res,next) => {
+    const {id} =req.params
+    // find job by id
+    const job = await jobsModel.findOne({_id:id})
+    // validation
+    if(!job){
+        next(`no job found with this id ${id}`)
+    }
+    if(!req.user.userId === job.createdBy.toString()){
+        next("You are not authorized to delete this job")
+        return
+    }
+    await job.deleteOne()
+    res.status(200).json({message:"Succes,Job deleted"})
+}
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import userAuth from "../middlewares/authMiddleware.js";
-import { createJobController, getAllJobsController } from "../controllers/jobsController.js";
+import { createJobController, deleteJobController, getAllJobsController, updateJobController } from "../controllers/jobsController.js";
 
 
 const router = express.Router();
@@ -11,5 +11,12 @@ router.post("/create-job",userAuth,createJobController)
 
 // get jobs || GET
 router.get('/get-job',userAuth,getAllJobsController)
+
+
+// UPDATE JOB || PUT || Patch
+router.patch("/update-job/:id",userAuth,updateJobController)
+
+// DELETE JOB || PUT || Patch
+router.delete("/delete-job/:id",userAuth,deleteJobController)
 
 export default router
